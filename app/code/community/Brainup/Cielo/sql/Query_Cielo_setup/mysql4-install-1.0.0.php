e777677e268972c5850877a600f1aa6e3c32f5a9
benrainir <benrainir@gmail.com>
2017-03-29 16:05:04 -0300

    create install script sql
    create install script sql

--- app/code/community/Brainup/Cielo/sql/Query_Cielo_setup/mysql4-install-1.0.0.php
+++ app/code/community/Brainup/Cielo/sql/Query_Cielo_setup/mysql4-install-1.0.0.php
@@ -5,8 +5,14 @@ $installer = Mage::getResourceModel('sales/setup', 'default_setup');
 
 $installer->startSetup();
 
+$setup = Mage::getResourceModel('sales/setup', 'default_setup');
 
-// criacao dos campos de juros para as entidades envolvidas na compra
+$tablePrefix = (string) Mage::getConfig()->getTablePrefix();
+if($tablePrefix)
+{
+	$tablePrefix = "_" . $tablePrefix;
+}
+
 
 $installer->addAttribute('order', 'base_interest', array
 (
@@ -54,6 +60,32 @@ $installer->addAttribute('creditmemo', 'interest', array
 (
 	'label' => 'Interest',
 	'type'  => 'decimal',
+));
+
+$installer->run
+("
+CREATE TABLE IF NOT EXISTS `" . $tablePrefix . "brainup_cielo_customer_token`
+(
+	`token_id` int(11) NOT NULL AUTO_INCREMENT,
+	`customer_id` int(10) unsigned DEFAULT NULL,
+	`token` varchar(255) NOT NULL,
+	`cc_type` varchar(255) NOT NULL,
+	`last_digits` varchar(255) NOT NULL,
+	PRIMARY KEY (`token_id`)
+)
+");
+
+
+$setup->addAttribute('quote_payment', 'cc_owner_doc', array
+(
+	'label' => 'Owner Document',
+	'type'  => 'varchar'
+));
+
+$setup->addAttribute('order_payment', 'cc_owner_doc', array
+(
+	'label' => 'Owner Document',
+	'type'  => 'varchar'
 ));
 
 $installer->endSetup();
